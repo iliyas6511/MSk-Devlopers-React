d6938de5e90cf6f4b04416b3c22936064e4e2fda
Iliyas Shiledar <iliyasshiledar313@gmail.com>
2025-08-11 11:23:13 +0530

    update layout

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Footer from './Footer'
 
 const Layout = () => {
+
+  const location = useLocation();
+
+  // Scroll to top on route change
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'instant', // Immediate scroll for new page load
+    });
+  }, [location.pathname]);
   return (
     <>
     <Navbar />
